5196e72d0a604f6147630657730728047f670d7b
alehunt <alehunt@gmail.com>
2023-09-15 20:43:03 -0300

    Reunion_14: Herencia de templates y estaticos

--- pig_23654/pig_23654/settings.py
+++ pig_23654/pig_23654/settings.py
@@ -115,7 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'juan/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'pedro', ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
