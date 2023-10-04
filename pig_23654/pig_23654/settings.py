d1e149d2db7c9d1e8f1899ff9b28e63e6cccbb55
alehunt <alehunt@gmail.com>
2023-10-04 19:24:30 -0300

    Reunion_19: Configuración de Base de Datos con Python-decouple y spoiler de un modelo

--- pig_23654/pig_23654/settings.py
+++ pig_23654/pig_23654/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dvp23#i34a28#f#-nk=@7@8_2_72$^a4t(!4jn%as_a-t^!nf4'
+# SECRET_KEY = 'django-insecure-dvp23#i34a28#f#-nk=@7@8_2_72$^a4t(!4jn%as_a-t^!nf4'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,12 +75,16 @@ WSGI_APPLICATION = 'pig_23654.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': config("DATABASE_HOST"),
+        'PORT': config("DATABASE_PORT",),
+        'NAME': config("DATABASE_NAME"),
+        'USER': config("DATABASE_USER"),
+        'PASSWORD': config("DATABASE_PASSWORD")
+    }
+}
 
 
 # Password validation
