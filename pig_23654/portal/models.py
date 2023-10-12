6f6d7ecfac6a26b9c23767be03127e598f757484
alehunt <alehunt@gmail.com>
2023-10-12 10:48:30 -0300

    Reunion_20 y Reunion_21: Equipos que hicieron exposición, y agregado de modelos a proyecto integrador, cuidado que no es incremental con el anterior commit, arranca desde cero en este

--- pig_23654/portal/models.py
+++ pig_23654/portal/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
-# Create your models here.
 class Persona(models.Model):
     nombre = models.CharField(verbose_name="Nombre", max_length=250)
+    apellido = models.TextField(verbose_name="Apellido")
+    dni = models.CharField(verbose_name="DNI", max_length=250, primary_key=True)
+    email = models.EmailField(verbose_name="Email", max_length=250)
+    apodo = models.CharField(verbose_name="Apodo", max_length=250)
+
+    def __str__(self) -> str:
+        return f"{self.dni} - {self.apellido}"
